69b6ba6ba70d0775bf36baf4aa9c5c1c0fa2c1da
salman rasheed <salmanR1122>
2024-05-28 02:33:57 -0700

    first commit

---
+++ header.php
@@ -0,0 +1,129 @@
+
+
+    <div class="wrapper">
+        
+    <!-- end loader -->
+
+        <div class="sidebar">
+            <!-- Sidebar  -->
+            <nav id="sidebar">
+
+                <div id="dismiss">
+                    <i class="fa fa-arrow-left"></i>
+                </div>
+
+                <ul class="list-unstyled components">
+
+                    <li class="active">
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#training">Training</a>
+                    </li>
+                    <li>
+                        <a href="#gallery">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="ok.php">ok</a>
+                    </li>
+                    <li>
+                       <a href="neworder.php">New Order</a>
+                    </li>
+                </ul>
+
+            </nav>
+        </div>
+
+        <div id="content">
+            <!-- header -->
+            <header>
+                <div class="container">
+                    <div class="row bor_bottom">
+                        <div class="col-md-3">
+                            <div class="full">
+                                <a class="logo" href="index.html"><img src="images/logo.png" alt="#" /></a>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+                            <ul class="social_top_icon">
+                                <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                            <div class="full">
+                                <div class="right_header_info">
+                                    <ul>
+                                        <li>
+                                            <button type="button" id="sidebarCollapse">
+                                                <img src="images/menu_icon.png" alt="#">
+                                            </button>
+                                        </li>
+                                        <li><img style="margin-right: 15px;" src="images/search_icon.png" alt="#"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- end header -->
+            <!-- start slider section -->
+            <div class="slider_section banner_main">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="main_text">
+                                <h1>Welcome <br><strong class="bold_text">D</strong><strong class="bold_text_black">Sport</strong></h1>
+                                <span>Contrary to popular belief</span>
+                                <a href="#">Read More <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end slider section -->
+
+            <!-- six_box-->
+            <div class="container">
+                <div class="six_box">
+                    <div class="row">
+                        <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 pppp">
+                            <div class="black_box">
+                             <li><a href="index.php"><h3>Home Page</h3></a></li>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 pppp">
+                            <div class="black_box yellow_box">
+                                <h3>Swimming</h3>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 pppp">
+                            <div class="black_box">
+                                <h3>Track Training</h3>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 pppp">
+                            <div class="black_box yellow_box">
+                                <h3>Golf</h3>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 pppp">
+                            <div class="black_box">
+                                <h3>Tennis</h3>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 pppp">
+                            <div class="black_box yellow_box">
+                            <li><a href="neworder.php"><h3>New Order</h3></a></li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
---
+++ ok.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>DSports</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- owl css -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- awesome fontfamily -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+</head>
+<!-- body -->
+
+<body class="main-layout">
+    <!-- loader  -->
+    <?php include "header.php"; ?>
+            <!-- end six_box-->
+
+            <!-- about -->
+            <div id="about" class="about">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                            <div class="about_img">
+                                <figure><img src="images/about-img.jpg" alt="#" /></figure>
+                            </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12 pa-ing">
+                            <div class="about_text">
+                                <div class="about_text_box">
+                                    <h2>About<strong class="black_white"> d sports</strong></h2>
+                                    <p>About Us Details</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end about -->
+
+            <!-- training -->
+            <div id="training" class="training">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="title">
+
+                                <h2>Exclusive<strong class="black"> training packages</strong></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="training_packages_bg">
+                        <div class="row">
+                            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                                <div class="training_packages_box">
+                                    <h3>SWIMMING COACH</h3>
+                                    <span>$<strong class="white_bold">85</strong></span>
+                                    <p>month</p>
+                                    <a class="BookNow" href="#">Book Now</a>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                                <div class="training_packages_box">
+                                    <h3>Tennis Champion</h3>
+                                    <span>$<strong class="white_bold">85</strong></span>
+                                    <p>month</p>
+                                    <a class="BookNow" href="#">Book Now</a>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                                <div class="training_packages_box">
+                                    <h3>Trekking Practice</h3>
+                                    <span>$<strong class="white_bold">85</strong></span>
+                                    <p>month</p>
+                                    <a class="BookNow" href="#">Book Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end training -->
+
+            <!-- Gallery -->
+            <div id="gallery" class="Gallery">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="title">
+                                <h2>Latest<strong class="black"> Gallery</strong></h2>
+                                <span>Packages Details</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                            <div class="row">
+                                <div class="col-md-12 mar_bottom">
+                                    <div class="Gallery_box">
+                                        <figure><img src="images/gallery1.jpg" alt="#" /></figure>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="Gallery_box">
+                                        <figure><img src="images/gallery4.jpg" alt="#" /></figure>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                            <div class="row">
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mar_bottom">
+                                    <div class="Gallery_box">
+                                        <figure><img src="images/gallery2.jpg" alt="#" /></figure>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mar_bottom">
+                                    <div class="Gallery_box">
+                                        <figure><img src="images/gallery3.jpg" alt="#" /></figure>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                    <div class="Gallery_box">
+                                        <figure><img src="images/gallery5.jpg" alt="#" /></figure>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                    <div class="Gallery_box">
+                                        <figure><img src="images/gallery6.jpg" alt="#" /></figure>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end Gallery -->
+
+            <!-- Reviwe -->
+            <div id="review" class="Reviwe">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="title">
+                                <h2>Review</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="Reviwe_box">
+                                <i><img src="images/title.png" alt="#"/></i>
+                                <h3>Jone due</h3>
+                                <p>My review here</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end Reviwe -->
+
+            <!-- footer -->
+            <div id="contact" class="contact">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="title">
+                                <h2>Contact<strong class="black"> Us</strong></h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid padddd">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 padddd">
+                    <div class="map_section">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+
+                                    <?php 
+									
+	    $cusname = $_REQUEST['customer_name'];
+		$cusphone = $_REQUEST['cus_phone'];
+		
+		/*
+		$con = mysqli_connect("localhost","root","vertrigo","db_neworder");
+		
+		$sql = "INSERT INTO contact_page SET cus_name='$cusname',";
+		
+		$rs = mysqli_query($con,$sql);
+		
+		if($rs==1)
+			echo "record inserted";
+		else
+			"checkkkkkkkkkkkkkkkkk";
+		*/
+		
+		
+		if((empty($uname)) || (empty($cusphone)))
+		{
+		echo "<h2><font color=red>Enter * Field</font></h2>";
+		}
+		else
+		{
+		echo "<h2>Customer Name;" .$cusname ."</h2>";
+        }
+		
+		?>
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									
+									?>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="map">
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+<?php include "footer.php"; ?>
+        <!-- end footer -->
+    </div>
+    </div>
+    <div class="overlay"></div>
+    <!-- Javascript files-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#sidebar").mCustomScrollbar({
+                theme: "minimal"
+            });
+
+            $('#dismiss, .overlay').on('click', function() {
+                $('#sidebar').removeClass('active');
+                $('.overlay').removeClass('active');
+            });
+
+            $('#sidebarCollapse').on('click', function() {
+                $('#sidebar').addClass('active');
+                $('.overlay').addClass('active');
+                $('.collapse.in').toggleClass('in');
+                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+            });
+        });
+    </script>
+    <script language="javascript">
+
+	function chr(e)
+{
+ascii = e.keyCode;
+//alert(ascii);
+if(((ascii >=65) && (ascii <=90)) ||((ascii >=97) && (ascii <=122)) || (ascii ==32))
+	return true;
+	else 
+	return false;
+}
+
+function numb(e)
+{
+ascii = e.keyCode;
+//alert(ascii);
+
+	if(((ascii >=48) && (ascii <=58)) || (ascii ==32))
+		return true;
+		else 
+		return false;
+}
+	</script>
+
+    <script>
+        // This example adds a marker to indicate the position of Bondi Beach in Sydney,
+        // Australia.
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 11,
+                center: {
+                    lat: 29.354349,
+                    lng: 71.691063
+                },
+            });
+
+            var image = 'images/maps-and-flags.png';
+            var beachMarker = new google.maps.Marker({
+                position: {
+                    lat: 29.354349,
+                    lng: 71.691063
+                },
+                map: map,
+                icon: image
+            });
+        }
+    </script>
+    <!-- google map js -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
+    <!-- end google map js -->
+
+</body>
+
+</html>
